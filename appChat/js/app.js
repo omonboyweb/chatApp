7c7a28e60292de4b62f91ef2915be6f3425afdf8
Omonbek <143828585+omonboyweb@users.noreply.github.com>
2023-12-11 16:38:19 +0500

    Add files via upload

--- appChat/js/app.js
+++ appChat/js/app.js
@@ -3,11 +3,20 @@ window.addEventListener("load", () => {
   let user = JSON.parse(localStorage.getItem("user"));
   if (!user) {
     window.location.href = "signIn.html";
+  } else {
+    myName.innerHTML = user.fullName;
+    myUsername.innerHTML = user.username;
+    userImage.setAttribute("src", user.img);
   }
 });
+const userImage = document.querySelector(".userImage");
+const myName = document.querySelector(".myName");
+const myUsername = document.querySelector(".myUsername");
+const logOut = document.querySelector(".logOut");
 const listIcon = document.querySelector(".listIcon");
 const menuBg = document.querySelector(".menuBg");
 const bgTogle = document.querySelector(".bgTogle");
+const upload = document.querySelector(".upload");
 
 listIcon.addEventListener("click", () => {
   menuBg.classList.add("menuBgg");
@@ -15,4 +24,14 @@ listIcon.addEventListener("click", () => {
 
 bgTogle.addEventListener("click", () => {
   menuBg.classList.remove("menuBgg");
+});
+
+logOut.addEventListener("click", () => {
+  localStorage.removeItem("user");
+  window.location.href = "signIn.html";
+});
+
+upload.addEventListener("change", () => {
+  const selectedFile = upload.files[0];
+  console.log(selectedFile);
 });
--- appChat/js/signUp.js
+++ appChat/js/signUp.js
@@ -3,14 +3,38 @@ const form = document.querySelector(".form");
 const fullName = document.querySelector(".fullName");
 const username = document.querySelector(".username");
 const password = document.querySelector(".password");
-const add = document.querySelector(".newAccount");
+let users = [];
+window.addEventListener("load", () => {
+  fetch("https://655f26f7879575426b44ad38.mockapi.io/users")
+    .then((res) => res.json())
+    .then((data) => {
+      users = data;
+    });
+});
 
-add.addEventListener("submit", (e) => {
+form.addEventListener("submit", (e) => {
   e.preventDefault();
   let user = {};
-  user.fullName == fullName.value;
-  user.username == username.value;
-  user.password == password.value;
+  user.fullName = fullName.value;
+  user.username = username.value;
+  user.password = password.value;
   user.img = "";
   user.id = null;
+
+  if (users.some((item) => item.username == user.username)) {
+    alert("Bunday foydalinuvchi mavjud");
+  } else {
+    fetch("https://655f26f7879575426b44ad38.mockapi.io/users", {
+      method: "POST",
+      body: JSON.stringify(user),
+      headers: {
+        "Content-type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        localStorage.setItem("user", JSON.stringify(data));
+        window.location.href = "index.html";
+      });
+  }
 });
